64e7a2894abc819011d2012ea0883fb081a9b9a3
tikendraw <tikendraksahu1029@gmail.com>
2024-01-10 16:41:48 +0530

    clean data script formatted

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,38 +1,42 @@
 import logging
+import os
+import pickle
 from datetime import datetime
 from pathlib import Path
 
+import missingno as msno
 import pandas as pd
 from box import ConfigBox
-from icecream import ic
 from ruamel.yaml import YAML
-# import yaml
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 
-# logging.basicConfig(filename='data_cleaning.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filemode='a')
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 logger.info(f"Started data preprocessing at {datetime.now()}.")
 
-# Load the training data
-def load_data(file_path):
-    df = pd.read_csv(file_path)
-    return df
+# Constants
+NULL_THRESHOLD = 0.10
+CARDINALITY_THRESHOLD= 0.80
 
-# Display the first few rows of the dataframe
-def display_head(df):
-    return df.head()
+# Data Loading
+def load_data(file_path:str | Path):
+    return pd.read_csv(file_path)
 
-# Visualize missing data using missingno library
-def visualize_missing_data(df):
-    import missingno as msno
-    msno.matrix(df)
+# Data Cleaning
+def remove_high_null_columns(df, threshold=NULL_THRESHOLD):
+    """
+    Remove columns with missing values exceeding a threshold.
 
-# Remove columns with missing values exceeding a threshold
-def remove_high_null_columns(df, threshold=0.10):
+    Parameters:
+    - df (pd.DataFrame): Input DataFrame.
+    - threshold (float): Threshold for null values.
+
+    Returns:
+    - pd.DataFrame: DataFrame after removing high null columns.
+    """
     null_percentage_count = {}
     high_null_counts = []
     
@@ -42,26 +46,21 @@ def remove_high_null_columns(df, threshold=0.10):
         if x > threshold:
             high_null_counts.append(column)
         
-        # print(f'{column:15} : {x}')
-    
     df.drop(high_null_counts, axis=1, inplace=True)
     return df
 
-# Remove duplicate rows
-def remove_duplicates(df):
-    df.drop_duplicates(inplace=True)
-    return df
-
-# Display information about the dataframe
-def display_info(df):
-    df.info()
-
-# Display summary statistics of the dataframe
-def display_statistics(df):
-    df.describe()
-
 # Remove columns with high cardinality
-def remove_high_cardinality_columns(df, threshold=0.80):
+def remove_high_cardinality_columns(df, threshold=CARDINALITY_THRESHOLD):
+    """
+    Remove columns with high cardinality.
+
+    Parameters:
+    - df (pd.DataFrame): Input DataFrame.
+    - threshold (float): Threshold for cardinality.
+
+    Returns:
+    - pd.DataFrame: DataFrame after removing high cardinality columns.
+    """
     cardinality_percentage_count = {}
     high_cardinal_columns = []
     
@@ -71,42 +70,63 @@ def remove_high_cardinality_columns(df, threshold=0.80):
         if x > threshold:
             high_cardinal_columns.append(column)
         
-        # print(f'{column:15} : {x:.3f} : {df[column].dtype}')
-    
     df.drop(high_cardinal_columns, axis=1, inplace=True)
     return df
 
 # Separate categorical and numerical columns
-def separate_categorical_numerical(df):
-    cat_df = df.select_dtypes(exclude='number')
-    num_df = df.select_dtypes(include='number')
-    return cat_df, num_df
+def get_categorical_numerical_columns(df):
+    """
+    Separate categorical and numerical columns in a DataFrame.
 
-# Display the number of unique values for each categorical column
-def display_categorical_unique_values(cat_df):
-    cat_df.nunique()
+    Parameters:
+    - df (pd.DataFrame): Input DataFrame.
 
-# Your main processing function
+    Returns:
+    - pd.DataFrame: DataFrame containing only categorical columns.
+    - pd.DataFrame: DataFrame containing only numerical columns.
+    """
+    cat_df = df.select_dtypes(exclude='number').columns
+    num_df = df.select_dtypes(include='number').columns
+    return { 
+            'categorical_columns':cat_df, 
+            'numerical_columns': num_df,
+            }
+
+# Preprocessing
 def main_processing(file_path:str, params):
-    df = load_data(file_path)
+    """
+    Main data processing function.
+
+    Parameters:
+    - file_path (str): Path to the input CSV file.
+    - params (ConfigBox): Configuration parameters.
+
+    Returns:
+    - pd.DataFrame: Original DataFrame.
+    - pd.DataFrame: Transformed DataFrame.
+    - Pipeline: Data processing pipeline.
+    """
+    original_df = load_data(file_path)
+    logger.info(f"Initial Dataframe shape: {original_df.shape}")
     
     # Data Cleaning
-    # visualize_missing_data(df)
-    df = remove_high_null_columns(df, threshold=params.data_cleaning.null_threshold)
-    df = remove_duplicates(df)
-    # display_info(df)
-    # display_statistics(df)
-    df = remove_high_cardinality_columns(df, threshold=params.data_cleaning.cardinality_threshold)
+    original_df = remove_high_null_columns(original_df, threshold=params.data_cleaning.null_threshold)
+    original_df.drop_duplicates(inplace=True)
+
     
-    # Preprocessing
-    cat_df, num_df = separate_categorical_numerical(df)
+    # Removing high cardinality columns
+    original_df = remove_high_cardinality_columns(original_df, threshold=params.data_cleaning.cardinality_threshold)
+    
+    # Separate categorical and numerical columns
+    columns = get_categorical_numerical_columns(original_df)
+    categorical_columns = columns['categorical_columns']
+    numerical_columns = columns['numerical_columns']
     
     preprocessor = ColumnTransformer(
         transformers=[
-            ('impute_null_nums', SimpleImputer(strategy='mean'), num_df.columns),
-            ('impute_null_cats', SimpleImputer(strategy='most_frequent'), cat_df.columns),  # You can use other strategies as well
-
-            ('one_hot_encode', OneHotEncoder(), cat_df.columns)
+            ('impute_null_nums', SimpleImputer(strategy='mean'), numerical_columns),
+            ('impute_null_cats', SimpleImputer(strategy='most_frequent'), categorical_columns ),
+            ('one_hot_encode', OneHotEncoder(), categorical_columns)
         ],
         remainder='passthrough', 
         verbose_feature_names_out=False,
@@ -118,37 +138,35 @@ def main_processing(file_path:str, params):
     ])
     
     
-
     # Apply the preprocessing steps to your DataFrame
-    df_transformed = pipeline.fit_transform(df)
+    df_transformed = pipeline.fit_transform(original_df)
 
     # Converting all the numpy data to pandas 
-    df_transformed = pd.DataFrame(df_transformed, columns = preprocessor.get_feature_names_out())
+    df_transformed = pd.DataFrame(df_transformed, columns=preprocessor.get_feature_names_out())
     logger.info(f'Shape after Data Transformation: {df_transformed.shape} ')
 
-    # dropping the categorical value that has been one hot encoded
-    df_transformed.drop(cat_df.columns, axis = 1, inplace=True)
+    # Dropping the categorical value that has been one hot encoded
+    df_transformed.drop(categorical_columns, axis=1, inplace=True)
     
-    logger.info(f"Dropping old categorical columns after OneHotEncoding : {cat_df.columns}")
+    logger.info(f"Dropping old categorical columns after OneHotEncoding: {categorical_columns}")
     logger.info(f'Shape after dropping categorical columns: {df_transformed.shape} ')
     
-    return df, df_transformed, preprocessor
+    return original_df, df_transformed, pipeline
 
-# Example usage
-if __name__=='__main__':
+# Execution
+if __name__ == '__main__':
     yaml = YAML(typ="safe")
 
+    # Load config and params
     params = ConfigBox(yaml.load(open("params.yaml", encoding="utf-8")))
+    config = ConfigBox(yaml.load(open("config.yaml", encoding="utf-8")))
+    
+    # Preprocess the data
+    df, df_transformed, pipeline = main_processing(config['train_filepath'], params=params)
 
-    import os
-    file_path = 'data/train.csv'
-    print(os.getcwd())
-    df, df_transformed, preprocessor = main_processing(file_path, params=params)
-    print('DATAFRAME')
-    print(type(df))
-
-    print(df.shape)
-    print('\n'*2)
-    print("TRANSFORMED")
-    print(type(df_transformed))
-    print(df_transformed.shape)
+    # Save the transformed data
+    df_transformed.to_csv(config['preprocessed_train_filepath'], index=False)
+    
+    # Save the processing pipeline as a pickle 
+    with open(config['preprocessing_pipeline'], 'wb') as file:
+        pickle.dump(pipeline, file) 
